ba4f0c31b84bfb770caea0f1feb821f8c1b83d29
Paloma <paloma.carmo@yahoo.com>
2026-03-25 09:11:13 -0300

    Prompt e Alert

---
+++ script.js
@@ -0,0 +1,26 @@
+// let = nomeUsuario = prompt('Digite seu nome:');
+
+// console.log("Mensagem de teste!")
+
+// let saudacao = 'Olá, mundo!'
+
+// console.log(saudacao);
+
+// alert('Opa!');
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let nome = prompt('Digite seu nome:');
+// let idade =  parseInt(prompt('Agora digite sua idade: '));
+// // let idadeNumber = parseInt(idade);
+// alert(`Seja bem-vindo, ${nome}! Vimos que tem ${idade} anos. `);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+let num1 = parseInt(prompt('Digite um número:'));
+let num2 = parseInt(prompt('Digite outro número:'));
+let soma = num1+num2;
+let produto = num1*num2;
+
+alert(`A soma dos números é ${soma} e o produto é ${produto}`);
